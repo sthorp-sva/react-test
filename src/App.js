61f187c697c12967d757d4654835ffd9b4b88487
Sam Thorp <sthorp@socialventures.org.au>
2026-05-07 17:21:36 +1000

    Basic message change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}, this is a test of a react app on Azure Static Web Apps</div>;
 }
 
 export default App;
