9de7c8022cafe02b907260fb9326b5b83393151c
Sam Thorp <sthorp@socialventures.org.au>
2026-05-12 11:54:46 +1000

    Add test message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}, this is a test of a react app on Azure Static Web Apps</div>;
+  return <div>Hello {value}, this is a test of a react app on Azure Static Web Apps, with authentication</div>;
 }
 
 export default App;
