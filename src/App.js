c742db23f4288cd820f8bcb3457ab27648fad868
Sam Thorp <sthorp@socialventures.org.au>
2026-05-14 11:26:07 +1000

    Message to test auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}, this is a test of a react app on Azure Static Web Apps, with authentication. Should be specific to SVA. Test build.</div>;
+  return <div>Hello {value}, this is a test of a react app on Azure Static Web Apps, with authentication. Should be specific to SVA. Test build. <p>Test of person-level restriction - should be open to Anders and Jack but NOT Alex. </p></div>;
 }
 
 export default App;
